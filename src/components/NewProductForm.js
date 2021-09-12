bbd5de248135fb854404a17c88c4ae64da3293e0
Simple Dimple John <simpledimplejohn@gmail.com>
2021-09-12 16:01:08 -0700

    update NewProductForm.js

--- src/components/NewProductForm.js
+++ src/components/NewProductForm.js
@@ -1,34 +1,35 @@
 import React from "react";
-import { v4 } from 'uuid';
-import PropTypes from "prop-types";
+import { v4 } from 'uuid'
+import PropTypes from "prop-types"
 import ReusableForm from "./ReusableForm";
-
-//props.onNewProductCreation() does not have access to .this in this commponent but does have access to props
-//onNewProductCreation() is the callback from the parent commponent its invoked in ProductControler.js wiht the name (newProduct) that will get passed into the handleAddingNewProductToList fx
 
 function NewProductForm(props) {
   function handleNewProductFormSubmission(event) {
-    event.preventDefault();
-    props.onNewProductCreation({
-      names: event.target.names.value, 
-      location: event.target.location.value, 
-      issue: event.target.issue.value, 
-      id: v4()
-    });
+    event.preventDefault()
+    props.onNewProductCreation(
+      {
+        name: event.target.name.value,
+        description: event.target.description.value,
+        quantity: event.target.quantity.value,
+        id: v4()
+      }
+    )
   }
 
   return (
-    //onSubmit  event handler triggers the fx
     <React.Fragment>
+      <p><strong>****VISIBLE, Add a new product, NewProductForm.js***</strong></p>
       <ReusableForm
         formSubmissionHandler={handleNewProductFormSubmission}
-        buttonText="Add to Cart!" />
+        formButtonText="Add Product **text in NewProductForm.js"
+        prodBool={false}
+      ></ReusableForm>
     </React.Fragment>
-  );
+  )
 }
 
 NewProductForm.propTypes = {
-  onNewProductCreation: PropTypes.func
-};
+  onNewProductCreation: PropTypes.func //inherited from parent. Function from controller handleaddingnewproducttolist
+}
 
-export default NewProductForm;
+export default NewProductForm
