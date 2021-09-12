5021d1c77743c88dd848775f00711f0470b7ca25
Simple Dimple John <simpledimplejohn@gmail.com>
2021-09-12 16:01:59 -0700

    update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,27 +1,62 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap"
 
-function Product(props){
+export default function Product(props) {
+  const prod = { name: props.name, description: props.description, quantity: props.quantity, id: props.id }
+
+  let subtract = "Subtract 1"
+  if (prod.quantity === 0) {
+    subtract = "Out of Stock"
+  }
+
   return (
     <React.Fragment>
-      <div onClick = { () => props.whenProductClicked(props.id)}>
-        <h3>{props.location} - {props.names}</h3>
-        <p><em>{props.issue}</em></p>
-        <hr/>
+      <p><strong>****Shows Product Details, subtract button, Product.js***</strong></p>
+      <div className="col-6 mb-3">
+        <Card>
+          <Card.Body>
+            <Card.Title onClick={() => props.whenProductClicked(props.id)}>{props.name}</Card.Title>
+            <Card.Text>
+              {props.description}
+            </Card.Text>
+            <ul>
+              <li>Quantity: {props.quantity}</li>
+              <li>ID: {props.id}</li>
+            </ul>
+            <p><strong>****add and subtract buttons here:***</strong></p>
+            <Button
+              type="submit"
+              variant="success"
+              className="m-3"
+              onClick={() => props.stockPlus(prod)}
+            >
+              Add 1 **in Product.js**
+            </Button>
+            
+            <Button
+              type="submit"
+              variant="warning"
+              className="m-3"
+              onClick={() => props.stockMinus(prod)}
+            >
+              {subtract}
+            </Button>
+
+          </Card.Body>
+        </Card>
       </div>
     </React.Fragment>
   );
 }
 
-Product.propTypes = {
-  names: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  issue: PropTypes.string.isRequired,
-  id: PropTypes.string,
-  whenProductClicked: PropTypes.func
-};
-//Product holds all the propTypes that will be passed down 
-//onClick pulls an individual product
-//onClick uses an arrow fx to prevent it from being called immediately
 
-export default Product;
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  quantity: PropTypes.number.isRequired,
+  id: PropTypes.string,
+  whenProductClicked: PropTypes.func,
+  stockPlus: PropTypes.func
+}
+
