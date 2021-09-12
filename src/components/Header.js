b620e2f9b397d86c6214d6d1a966c33672d336ec
Simple Dimple John <simpledimplejohn@gmail.com>
2021-09-12 16:52:14 -0700

    update text in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function Header(){
   return (
-    <h1>Tap Room</h1>
+    <React.Fragment>
+      <h1>Tap Room</h1>
+      <h2>Wholesale Kombucha</h2>
+      <p>Keg's Available for purchase</p>
+    </React.Fragment>
+
   );
 }
 
