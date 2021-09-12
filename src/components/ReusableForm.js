571bc37956bcadbdd1592ab1cf45c6c0c8218c69
Simple Dimple John <simpledimplejohn@gmail.com>
2021-09-12 16:04:15 -0700

    update ReusableForm.js

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,30 +1,58 @@
 import React from "react";
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"
+import { Form, Button } from 'react-bootstrap'
 
 function ReusableForm(props) {
   return (
     <React.Fragment>
-      <form onSubmit={props.formSubmissionHandler}>
-        <input
-          types='text'
-          name='names'
-          placeholder='Pair Names' />
-        <input
-          type='text'
-          name='location'
-          placeholder='Location' />
-        <textarea
-          name='issue'
-          placeholder='Describe your issue.' />
-        <button type='submit'>{props.buttonText}</button>
-      </form>
+      <Form onSubmit={props.formSubmissionHandler}>
+        <Form.Group
+          className="mb-3"
+          controlId="productName">
+          <Form.Label>Product Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            required
+            defaultValue={props.prodBool ? props.product.name : ""}
+          >
+          </Form.Control>
+        </Form.Group>
+        <Form.Group
+          className="mb-3"
+          controlId="productDescription">
+          <Form.Label>Product Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            name="description"
+            defaultValue={props.prodBool ? props.product.description : ""}
+            required />
+        </Form.Group>
+        <Form.Group
+          className="mb-3"
+          controlId="productQuantity">
+          <Form.Label>Quantity</Form.Label>
+          <Form.Control
+            type="number"
+            name="quantity"
+            min="0"
+            defaultValue={props.prodBool ? props.product.quantity : 0}
+            required />
+        </Form.Group>
+        <Button
+          variant="primary"
+          type="submit">
+          {props.formButtonText} **button in ProductForm.js**
+        </Button>
+      </Form>
     </React.Fragment>
-  );
+  )
 }
+
 
 ReusableForm.propTypes = {
-  formSubmissionHandler: PropTypes.func,
-  button: PropTypes.string
+  onNewProductCreation: PropTypes.func //inherited from parent. Function from controller handleaddingnewproducttolist
 }
 
-export default ReusableForm;
+export default ReusableForm
