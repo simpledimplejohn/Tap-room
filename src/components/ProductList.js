5c4ceeb28ea8fff6a6092ee44016df8156f1c115
Simple Dimple John <simpledimplejohn@gmail.com>
2021-09-12 16:03:43 -0700

    update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,19 +1,20 @@
 import React from "react";
-import Product from "./Product";
-import PropTypes from "prop-types";
+import Product from "./Product"
+import PropTypes from "prop-types"
 
-function ProductList(props){
+function ProductList(props) {
   return (
     <React.Fragment>
-      <hr/>
+      <p><strong>****NOTHING HERE: just map fx, ProductList.js***</strong></p>
       {props.productList.map((product) =>
-        <Product 
-          whenProductClicked = { props.onProductSelection }
-          names={product.names}
-          location={product.location}
-          issue={product.issue}
+        <Product name={product.name}
+          whenProductClicked={props.onProductSelection}
+          stockPlus={props.addStock}
+          stockMinus={props.subtractStock}
+          description={product.description}
+          quantity={product.quantity}
           id={product.id}
-          key={product.id}/>
+          key={product.id} />
       )}
     </React.Fragment>
   );
@@ -21,10 +22,9 @@ function ProductList(props){
 
 ProductList.propTypes = {
   productList: PropTypes.array,
-  onProductSelection: PropTypes.func
-};
-//this passes down the propTypes:
-// productList the array
-// & onProductSelection the function determines whether a product has been selected
+  onProductSelection: PropTypes.func,
+  addStock: PropTypes.func,
+  subtractStock: PropTypes.func
+}
 
-export default ProductList;
+export default ProductList
