ebe6f492c432d24b4d8d3e0d3171948fecab656e
Simple Dimple John <simpledimplejohn@gmail.com>
2021-09-12 16:45:09 -0700

    update Products.js onClick location

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, Card } from "react-bootstrap"
+import { Button } from "react-bootstrap"
 
 export default function Product(props) {
   const prod = { name: props.name, description: props.description, quantity: props.quantity, id: props.id }
@@ -13,38 +13,25 @@ export default function Product(props) {
   return (
     <React.Fragment>
       <p><strong>****Shows Product Details, subtract button, Product.js***</strong></p>
-      <div className="col-6 mb-3">
-        <Card>
-          <Card.Body>
-            <Card.Title onClick={() => props.whenProductClicked(props.id)}>{props.name}</Card.Title>
-            <Card.Text>
-              {props.description}
-            </Card.Text>
-            <ul>
-              <li>Quantity: {props.quantity}</li>
-              <li>ID: {props.id}</li>
-            </ul>
-            <p><strong>****add and subtract buttons here:***</strong></p>
-            <Button
-              type="submit"
-              variant="success"
-              className="m-3"
-              onClick={() => props.stockPlus(prod)}
-            >
-              Add 1 **in Product.js**
-            </Button>
-            
-            <Button
-              type="submit"
-              variant="warning"
-              className="m-3"
-              onClick={() => props.stockMinus(prod)}
-            >
-              {subtract}
-            </Button>
-
-          </Card.Body>
-        </Card>
+      <div onClick={() => props.whenProductClicked(props.id)}>
+        <p>{props.name}</p>
+        <p>
+          {props.description}
+        </p>
+        <ul>
+          <li>Quantity: {props.quantity}</li>
+          <li>ID: {props.id}</li>
+        </ul>
+        <p><strong>****add and subtract buttons here:***</strong></p>
+        <Button
+          type="submit" onClick={() => props.stockPlus(prod)}>
+          Add 1 **in Product.js**
+        </Button>
+        
+        <Button
+          type="submit" onClick={() => props.stockMinus(prod)}>
+          {subtract}
+        </Button>
       </div>
     </React.Fragment>
   );
